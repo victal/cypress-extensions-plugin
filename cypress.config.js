532a0d2c110ea815e189f2dd8da14147716f3b6c
Guilherme Victal <guilhermevictal@gmail.com>
2023-02-13 13:30:58 -0300

    test: disable video and screenshots on tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
